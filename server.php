329d28381af576592c480f3a68b7c7d5f59a7d18
Mohammad Shahadath Hossain <33853447+ronibd0@users.noreply.github.com>
2020-09-06 14:15:00 +0600

    Update server.php

--- server.php
+++ server.php
@@ -4,7 +4,7 @@
  * Laravel - A PHP Framework For Web Artisans
  *
  * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
+ * @author   go communication <info@goisp.net>
  */
 
 $uri = urldecode(
